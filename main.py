12b4bfb36a0c99723c8fd1bb23a6378a3cc44372
mdhabeebvulla <55741151+mdhabeebvulla@users.noreply.github.com>
2024-11-19 18:24:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.datasets import load_iris
 from pydantic import BaseModel
 import numpy as np
-
+import os
 app = FastAPI()
 
 # Load Iris data
@@ -23,3 +23,6 @@ def predict_iris(features: IrisFeatures):
     features_array = np.array([[features.sepal_length, features.sepal_width, features.petal_length, features.petal_width]])
     prediction = model.predict(features_array)
     return {"class": iris.target_names[prediction[0]]}
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 8000))  # Default to port 8000
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
